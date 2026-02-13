c2310f61b551f8644529e04a804cb51157a2eab4
Pouzinnn <britexjohn@gmail.com>
2026-02-13 19:52:55 -0300

    Create Atividade3if.py

---
+++ Atividade3if.py
@@ -0,0 +1,9 @@
+hovendo = False
+nublado = True
+if chovendo == True :
+  print "levar guarda chuva" 
+else :
+  if nublado == True :
+    print "levar guarda chuva mesmo assim" 
+  else :
+    print "nao levar guarda chuva "
