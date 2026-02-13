cdf3f779baf67058daaf866303c7b074a7e78337
Pouzinnn <britexjohn@gmail.com>
2026-02-13 20:29:08 -0300

    Create Activity4if.py

---
+++ Activity4if.py
@@ -0,0 +1,10 @@
+SapoMorreu = True
+GuinomioEscapou = False
+if SapoMorreu == True :
+  print "corre guinomio, CORRE" 
+else :
+  print "ELE TA VIVOOO"
+  if GuinomioEscapou == True :
+    print "Gloriaaaaaaaaaaaaa"
+  else :
+      print "coitado"
