ad07c65a703ffaaf45b283c7e5a11b922117e13d
Pouzinnn <britexjohn@gmail.com>
2026-02-13 19:28:16 -0300

    Create Atividade2if.py

---
+++ Atividade2if.py
@@ -0,0 +1,8 @@
+flechas = 10
+vida = 10
+if flechas > vida :
+  print "inimigo foi assasinado"
+if vida > flechas :
+   print "tem um homem mamaco correndo atras de mim, aaaaaaaaaaa"
+if vida == flechas :
+  print "eu ganhei e eu virei CLT"
