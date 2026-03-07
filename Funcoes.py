9b4cf4bad3be3466d504d39d874523fbb7a25ad3
Pouzinnn <britexjohn@gmail.com>
2026-03-06 21:12:32 -0300

    Create Funcoes.py

---
+++ Funcoes.py
@@ -0,0 +1,17 @@
+def diminuir(a,b):
+    resultado = a-b
+    return resultado
+
+print(diminuir(60,47))
+
+def multiplicar(a,b):
+    resultado = a*b
+    return resultado
+
+print(multiplicar(60,47))
+
+def dividir(a,b):
+    resultado = a/b
+    return resultado
+
+print(dividir(30,3))
